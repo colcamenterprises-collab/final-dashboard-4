4b8cc713de64e421605840b9738772a4d5b0f5a5
colcamenterprises-collab <colcamenterprises@gmail.com>
2026-07-11 21:39:53 +0700

    Compose expenses page with personal table

--- client/src/pages/finance/ExpensesPersonal.tsx
+++ client/src/pages/finance/ExpensesPersonal.tsx
@@ -1,1 +1,13 @@
-export { default } from "./Expenses";
+import Expenses from "./Expenses";
+import { PersonalExpensesTable } from "@/components/PersonalExpensesTable";
+
+export default function ExpensesPersonal() {
+  return (
+    <>
+      <Expenses />
+      <div className="mx-auto max-w-7xl p-4 pt-0">
+        <PersonalExpensesTable dateFrom="" dateTo="" />
+      </div>
+    </>
+  );
+}
