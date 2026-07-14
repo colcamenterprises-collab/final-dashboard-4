f4113a8613a732982dad897cc9ea2728af19bf1a
colcamenterprises-collab <colcamenterprises@gmail.com>
2026-07-14 23:18:12 +0700

    feat(pos): place POS routes in dashboard shell

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -51,6 +51,8 @@ import AdminOrders from "./pages/admin/ordering/AdminOrders";
 import AdminSettings from "./pages/admin/ordering/AdminSettings";
 import AdminQrCodes from "./pages/admin/ordering/AdminQrCodes";
 import PosRegister from "./pages/pos/PosRegister";
+import PosKitchen from "./pages/pos/PosKitchen";
+import PosDisplay from "./pages/pos/PosDisplay";
 
 import StaffDashboard from "./pages/staff/Dashboard";
 import StaffMembers from "./pages/staff/Members";
@@ -88,8 +90,10 @@ export default function App() {
                   <Route path="/order/status/:orderId" element={<OrderStatus />} />
                   <Route path="/ordering/tablet" element={<OrderPage tablet />} />
                   <Route path="/kitchen/display" element={<KitchenDisplay />} />
-                  <Route path="/pos" element={<PosRegister />} />
                   <Route element={<PageShell />}>
+                    <Route path="/pos" element={<ProtectedRoute><PosRegister /></ProtectedRoute>} />
+                    <Route path="/pos/kitchen" element={<ProtectedRoute><PosKitchen /></ProtectedRoute>} />
+                    <Route path="/pos/display" element={<PosDisplay />} />
                     <Route path="/dashboard" element={<ProtectedRoute><Home /></ProtectedRoute>} />
                     <Route path="/operations/daily-sales" element={<ProtectedRoute><DailySalesForm /></ProtectedRoute>} />
                     <Route path="/operations/daily-sales/edit/:id" element={<ProtectedRoute><DailySalesForm /></ProtectedRoute>} />
