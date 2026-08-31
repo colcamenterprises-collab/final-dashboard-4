70c9dd030dc3727bc475a483175e0ab8a0962714
colcamenterprises-collab <colcamenterprises@gmail.com>
2026-08-31 17:16:49 +0700

    Remove cross-owned POS shift foreign key from cutover

--- migrations/20260831_pos_shift_schema_cutover.sql
+++ migrations/20260831_pos_shift_schema_cutover.sql
@@ -3,22 +3,6 @@ BEGIN;
 ALTER TABLE public.ordering_orders
   ADD COLUMN IF NOT EXISTS pos_shift_id uuid;
 
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM pg_constraint
-    WHERE conrelid = 'public.ordering_orders'::regclass
-      AND conname = 'ordering_orders_pos_shift_id_fkey'
-  ) THEN
-    ALTER TABLE public.ordering_orders
-      ADD CONSTRAINT ordering_orders_pos_shift_id_fkey
-      FOREIGN KEY (pos_shift_id)
-      REFERENCES public.pos_shifts(id)
-      ON DELETE SET NULL;
-  END IF;
-END $$;
-
 DROP INDEX IF EXISTS public.ordering_orders_grab_order_number_unique;
 DROP INDEX IF EXISTS public.ordering_orders_ticket_number_unique;
 
